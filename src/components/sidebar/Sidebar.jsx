d8dd8e26bb7eef660d6848c4b1d69599104c23cd
Nahom1927 <yheys898184@gmail.com>
2023-06-02 00:20:38 +0300

    all

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './sidebar.css';
+import Logo from '../../assets/logo.svg';
+
+const Sidebar = () => {
+  return (
+    <aside className='aside'>
+      <a href='#home' className='nav__logo'>
+        <img src={Logo} alt=''/>
+      </a>
+
+      <nav className='nav'>
+        <div className='nav__menu'>
+          <ul className='nav__list'>
+            <li className='nav__item'>
+              <a href='#home' className='nav__link'>
+                <i className='icon-home'></i>
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#about' className='nav__link'>
+                <i className='icon-user-following'></i>
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#services' className='nav__link'>
+                <i className='icon-briefcase'></i>
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#resume' className='nav__link'>
+                <i className='icon-graduation'></i>
+              </a>
+              </li>
+            <li className='nav__item'>
+              <a href='#portfolio' className='nav__link'>
+                <i className='icon-layers'></i>
+              </a>
+              </li>
+            <li className='nav__item'>
+              <a href='#contact' className='nav__link'>
+                <i className='icon-bubbles'></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <footer className='nav__footer'>
+        <span className='copyright'>&copy; 2023 - 2024.</span>
+      </footer>
+    </aside>
+  )
+}
+
+export default Sidebar
